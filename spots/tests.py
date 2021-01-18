58e903e76b3c9b80547841fc0ae634ccc38b8655
Mark Mansolino <markjm610@gmail.com>
2021-01-18 01:14:24 -0500

    Clearer tests

--- spots/tests.py
+++ spots/tests.py
@@ -10,7 +10,11 @@ class GridTests(TestCase):
     def test_create_grid(self):
         client = APIClient()
         response = client.post('/grids/', {}, format='json')
-        # test that database has the grid
+
+        # Grid object got added to database
+        self.assertEqual(len(Grid.objects.all()), 1)
+
+        # Response status code and correct JSON in response
         self.assertEqual(response.status_code, 201)
         self.assertEquals(response.json()['animation_order'], '')
 
@@ -19,6 +23,8 @@ class GridTests(TestCase):
         test_grid.save()
         client = APIClient()
         response = client.get(f'/grids/{test_grid.id}')
+
+        # Response status code and correct JSON in response, meaning correct grid is retrieved
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), test_grid.to_dict())
 
@@ -32,9 +38,13 @@ class GridTests(TestCase):
 
         # Animation order gets updated, but ID doesn't
         self.assertEqual(test_grid.id, updated_grid.id)
+        self.assertNotEqual(test_grid.animation_order,
+                            updated_grid.animation_order)
+
+        # Animation order is updated to correct value
         self.assertEqual(updated_grid.animation_order, '12345')
 
-        # Response status code and correct JSON
+        # Response status code and correct JSON in response
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), updated_grid.to_dict())
         self.assertEqual(
