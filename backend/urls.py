9ebc79bd4ec31afa6736b67e7b2665dbe011b2a7
Mark Mansolino <markjm610@gmail.com>
2021-01-20 02:20:16 -0500

    HttpResponse at root URL

--- backend/urls.py
+++ backend/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
+    path('', views.server_up),
     path('admin/', admin.site.urls),
     path('grids/', include('spots.urls'))
 ]
